1f236d58017c91ef5d006a1d8d08e32f753557f3
Olena Kachmar <olena.kachmar9@gmail.com>
2019-02-15 14:47:51 +0000

    final

--- js/colorChangingButton.js
+++ js/colorChangingButton.js
@@ -1,12 +1,11 @@
 function getCookie(name) {
     let value = "; " + document.cookie;
     let parts = value.split("; " + name + "=");
-    if (parts.length == 2)
+    if (parts.length === 2)
         return parts.pop().split(";").shift();
 }
 
 $(document).ready(function() {
-    console.log("Color: " + getCookie("color"));
     $(".changeColor").css("background-color", getCookie("color"));
 });
 
